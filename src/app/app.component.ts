7e72f26b8725298af11e7242f16093aaa4067e2f
Alexey Mikhailov <krummi1414@yandex.ru>
2024-07-15 13:59:41 +0300

    feat: add structure FSD, install starting dependency TaigaUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TuiRootModule } from "@taiga-ui/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TuiRootModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
