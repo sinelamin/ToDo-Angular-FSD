14b681fd5aa1ef94232e8af3c56ed3c9e1f061dc
Alexey Mikhailov <krummi1414@yandex.ru>
2024-07-17 22:05:40 +0300

    feat: add services and methods for work with data base Strapi

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 import { TuiRootModule } from "@taiga-ui/core";
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
 
 import { routes } from './app.routes';
 
@@ -9,6 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideRouter(routes),
+    provideHttpClient(),
     importProvidersFrom(TuiRootModule)
   ]
 };
